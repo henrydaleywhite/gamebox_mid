ccc1b597397a2eab7365b252b603eaec205057a1
henrydaleywhite <henry.daley.white@gmail.com>
2019-02-12 13:58:15 -0500

    finish initial refactoring

--- run/src/controllers/private.py
+++ run/src/controllers/private.py
@@ -13,7 +13,7 @@ controller = Blueprint('private',__name__)
 @controller.route('/dashboard', methods=['GET','POST'])
 def dashboard():
     if request.method == 'GET':
-        # receive json with key user info which contains a dict with 
+        # receive json with key user_info which contains a dict with 
         # keys 'username', 'pk', 'email', and 'display_name'
         user_credentials = request.get_json()
         user = User(row=user_credentials['user_info'])
@@ -31,42 +31,45 @@ def dashboard():
 @controller.route('/setup', methods=['GET','POST'])
 def setup():
     if request.method == 'GET':
-        user = User(row=session['user_dict'])
-        game_params = user.game_start_params(pk=session['to_start'])
-        # search for users to invite
-        # choose the number of players/turn order
-        return render_template('setup.html', params=game_params)
+        """receive json with keys game_pk and user info :
+        user_info contains a dict with keys 'username', 'pk',
+        'email', and 'display_name'.
+        game_pk is the pk of the game from the available_games table"""
+        request = request.get_json()
+        user = User(row=request['user_info'])
+        game_params = user.game_start_params(pk=request['game_pk'])
+        return jsonify({'game_params': game_params})
     elif request.method == 'POST':
-        user = User(row=session['user_dict'])
-        user_list = [user.pk]
-        game_pk = request.form['game_pk']
-        max_num = int(request.form['max_num'])
-        for i in range(1, max_num):
-            form_req = 'player ' + str(i)
-            username = request.form.get(form_req)
-            if username:
-                player_pk = get_pk_from_username(username)
-                user_list.append(player_pk)
-        session['game'] = user.make_game(game_pk, user_list)
-        # request.form['button']
-        # use make_game method
-        # set session['game'] off of the new entries
-        return redirect(url_for('private.gamepage'))
+        """receive json with keys game_params and user info :
+        user_info contains a dict with keys 'username', 'pk',
+        'email', and 'display_name'.
+        game_params contains a dict with keys game_pk and user_list
+        user_list has the pks of all users playing the game"""
+        request = request.get_json()
+        user = User(row=request['user_info'])
+        user_list = request['game_params']['user_list']
+        game_pk = request['game_params']['game_pk']
+        game_id = user.make_game(game_pk, user_list)
+        return jsonify({'game_id': game_id})
 
 
 @controller.route('/gamepage', methods=['GET','POST'])
 def gamepage():
     if request.method == 'GET':
-        user = User(row=session['user_dict'])
-        game_pk = user.game_pk_from_id(session['game'])
+        """receive json with keys game_id and user info :
+        user_info contains a dict with keys 'username', 'pk',
+        'email', and 'display_name'.
+        game_id is the unique id of the game from the available_games table
+        """
+        request = request.get_json()
+        user = User(row=request['user_info'])
+        game_pk = user.game_pk_from_id(request['game_id'])
         game = GameStatus(pk=game_pk)
         game_state = game.game_state
         turn_order = game.turn_order.split(',')
         num_players = len(turn_order)
         turn_number = game.turn_number
         user_turn_id = turn_order[(turn_number % num_players)]
-        print(type(user_turn_id))
-        print(type(user.pk))
         if user_turn_id == str(user.pk):
             user_turn = "Yes"
         else:
@@ -81,22 +84,26 @@ def gamepage():
                                     }
                                 )
         html_to_pass = response.text
-        return render_template('game.html', html_to_pass = html_to_pass,
-                                user=user.username)
+        return jsonify({'html': html_to_pass})
+        
     elif request.method == 'POST':
-        user = User(row=session['user_dict'])
-        game_pk = user.game_pk_from_id(session['game'])
+        """receive json with keys game_params and user info :
+        user_info contains a dict with keys 'username', 'pk',
+        'email', and 'display_name'.
+        game_params contains a dict with keys 'game_id' and 'next_state'
+        game_id is the unique id of the game from the available_games table
+        next_state is a string representing the state of the game after the
+        most recent move"""
+        request = request.get_json()
+        user = User(row=request['user_info'])
+        game_pk = user.game_pk_from_id(request['game_params']['game_id'])
         game = GameStatus(pk=game_pk)
         endpoint = game.endpoint
-        game.game_state = request.form['next_state']
-        # increment turn number for each move made
+        game.game_state = request['game_params']['next_state']
         game.turn_number += 1
-        # get the game state back after being updated for the post request
-        post_response = requests.post(f"{endpoint}post", 
-                                        json = {'state': game.game_state})
         # update DB for new info
         game.save()
         # win state will have the string "WIN -" in it
         if game.game_state.find("WIN -") > -1:
-            return redirect(url_for('game_end_page'))
-        return redirect(url_for('private.gamepage'))
+            return jsonify({'win': True})
+        return jsonify({'win': False})
